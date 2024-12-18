edcdd704e16d6bd0cfc1a035ffb1d491769da039
Wendeltrs <wendeljt17@gmail.com>
2024-12-17 21:36:47 -0300

    feat: learning useCallback, useMemo, and useRef

--- src/app/pages/(login)/Login.tsx
+++ src/app/pages/(login)/Login.tsx
@@ -1,26 +1,39 @@
-import { useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleClick = () => {
+    const emailLength = useMemo(() => {
+        return email.length
+    }, [email]);
+
+    const handleClick = useCallback(() => {
         console.log(email);
         console.log(password);        
-    }
+    }, [email, password]);
     
     return (
         <>
             <div>
+                <p>Quantidade de caracteres do email: {emailLength}</p>
                 <form>
                     <label htmlFor="">
                         <span>Email</span>
-                        <input type="text" onChange={e => setEmail(e.target.value)}/>
+                        <input 
+                            type="text" 
+                            onChange={e => setEmail(e.target.value)}
+                            onKeyDown={e => e.key === "Enter" ? inputPasswordRef.current?.focus() : null}
+                        />
                     </label>
 
                     <label htmlFor="">
                         <span>Senha</span>
-                        <input type="password" onChange={e => setPassword(e.target.value)}/>
+                        <input 
+                            type="password" 
+                            onChange={e => setPassword(e.target.value)}
+                            ref={inputPasswordRef}/>
                     </label>
 
                     <button onClick={handleClick} type="button">Entrar</button>
